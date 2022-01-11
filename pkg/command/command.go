c0f59c4913b7369a8eb770dded1ca84e1c055111
zoulls <zanamixx@gmail.com>
2022-01-11 00:12:58 -0500

    Add uptime in admin command. Fix typo

---
+++ pkg/command/command.go
@@ -0,0 +1,12 @@
+package command
+
+import (
+	"time"
+
+	"github.com/zoulls/provencal-le-gaulois/config"
+)
+
+func GetUptime() time.Duration {
+	conf := config.GetConfig()
+	return time.Until(conf.GetStartDate())
+}
